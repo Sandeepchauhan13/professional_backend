5ee01a2d9d39c221481c9001ba8fe2899078d194
apnainstitute-demo <sinh.pozta1@gmail.com>
2024-01-23 11:49:21 +0530

    usercontrollers, getuserdetails, validation, check for user, checkforimages, upload on cloudinary etc

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {User} from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 // register user method 
 // asynchandler higher order function jo accept krta hia function ko 
 
@@ -40,7 +42,45 @@ const registerUser = asyncHandler( async (req, res)=>{
         throw new ApiError(409, "User with email or username already exists")
     }
 
-    req.files?.avatar[0]?.path
+//   local path of avatar and cover  images, req.files from multer 
+// check for images , check for avatar
+ const avatarLocalPath = req.files?.avatar[0]?.path;
+ const coverImageLocalPath = req.files?.coverImage[0]?.path;
+ if(!avatarLocalPath){
+throw new ApiError (400, "Avatar file is required");
+ }
+
+//  uploadimage on cloudinary 
+
+const avatar = await uploadOnCloudinary (avatarLocalPath)
+const coverImage = await uploadOnCloudinary (coverImageLocalPath)
+if(!avatar){
+    throw new ApiError(400, "Avatar file is required");
+}
+
+    // create user object - create entry in db 
+const user = await User.create({
+fullName,
+avatar: avatar.url,
+coverImage: coverImage?.url || "",
+email,
+ password,
+ username: username.toLowerCase()
+})
+// check for user creation & remove password and refresh token field from response
+const createdUser = await User.findById(user._id).select(
+    // kya kya nahi chahiye 
+    "-password -refreshToken"
+)
+if(!createdUser){
+    throw new ApiError(500, "Something went wrong while registering a user")
+
+}
+// return res 
+return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered Successfully")
+)
+
 
 } )
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,7 +58,7 @@ const userSchema = new Schema(
 // ye logic password encrypt kar dega 
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
